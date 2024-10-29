c39304d5b1db9bef3349bfd7d6d2b2e11db95420
Suffer-qw <stas.eryomchenko@gmail.com>
2024-10-29 11:37:02 +0700

    Update dz2910.cpp

--- dz2910.cpp
+++ dz2910.cpp
@@ -5,11 +5,19 @@ using namespace std;
 
 void romb(float& y, float& x)
 {
-	if ((y >= -0.5*x - 1 && y <= 0.5*x + 1) or (1 > pow(y,2) + pow(x, 2) && x>=0)) {
-		cout << "verno";
+	int z=0;
+	if ((y >= -0.5*x - 1 && y <= 0.5*x + 1 && x <= 1) or (1 >= pow(y,2) + pow(x, 2) && x>= 0 )) {
+		z = z + 1;
+	}
+	cout << " ";
+	if (y <= 1) {
+		z = z + 1;
+	}
+	if (z == 2) {
+		cout << "verno,";
 	}
 	else {
-		cout << "not verno";
+		cout << "not verno,";
 	}
 }
 /*void romb(float& y, float& x)
@@ -27,6 +35,32 @@ int main()
 {
 	float y;
 	float x;
-	cin >> y >> x;
-	romb(y, x);
+	for (int i = 0; i < 5; i++) {
+		switch (i) {
+		case(0):
+			y = -0.1;
+			x = -2.5;
+			break;
+		case(1):
+			y = 0.1;
+			x = -1.5;
+			break;
+		case(2):
+			y = 0.5;
+			x = -0.5;
+			break;
+		case(3):
+			y = 0.5;
+			x = 1.5;
+			break;
+		case(4):
+			y = 1.1;
+			x = 0.5;
+			break;
+		}
+		romb(y, x);
+	}
+	
+
+	
 }
