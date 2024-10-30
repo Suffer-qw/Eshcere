e4bb9ff090bc39a0a331ae82356d36ebb4128daf
Suffer-qw <stas.eryomchenko@gmail.com>
2024-10-30 19:24:32 +0700

    Create DZ511One.cpp

---
+++ DZ511One.cpp
@@ -0,0 +1,13 @@
+//DZ511One.cpp
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float r, rr, rrr;
+    cin >> r >> rr >> rrr;
+
+    cout << 1 / (1 / r + 1 / rr + 1 / rrr);
+}
