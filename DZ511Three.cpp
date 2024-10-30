befadbfdd85cc1ad3751c7f8b6334871fd745db5
Suffer-qw <stas.eryomchenko@gmail.com>
2024-10-30 19:37:16 +0700

    Create DZ511Three.cpp

---
+++ DZ511Three.cpp
@@ -0,0 +1,15 @@
+//DZ511Three.cpp
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    float a, b, c;
+    cin >> a >> b >> c;
+    if (a > b && b > c ) {
+        cout << "true";
+    }
+    
+}
