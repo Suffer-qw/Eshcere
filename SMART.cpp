4a9a60ec5577572ee4dbe41abb55169eef59195c
Suffer-qw <stas.eryomchenko@gmail.com>
2024-11-01 17:57:37 +0700

    Create SMART.cpp

---
+++ SMART.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void chisla(string& y )
+{
+    int numOne= stoi(y.substr(0, 1));
+    string oper = y.substr(1, 1);
+    int numTwo = stoi(y.substr(2, 2));
+    if (oper == "+") {
+        cout << numOne + numTwo;
+    }
+    if (oper == "-") {
+        cout << numOne - numTwo;
+    }
+    if (oper == "*") {
+        cout << numOne * numTwo;
+    }
+    if (oper == "/") {
+        cout << numOne / numTwo;
+    }
+    cout << oper;
+}
+
+int main()
+{
+    string a;
+    cin >> a ;
+    chisla(a);
+}
