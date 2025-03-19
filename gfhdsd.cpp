fa4e790f3c80271dab92c95b775419ceb6cc896f
Suffer-qw <stas.eryomchenko@gmail.com>
2025-03-19 10:35:22 +0700

    Update gfhdsd.cpp

--- gfhdsd.cpp
+++ gfhdsd.cpp
@@ -1,5 +1,5 @@
 // ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Я НЕ ПРИДУМАЛ КАК РЕАЛИЗОВАТЬ row
 
 #include <iostream>
 #include <stdlib.h>
